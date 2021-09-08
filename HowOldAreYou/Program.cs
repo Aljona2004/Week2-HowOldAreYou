84d596146e70ef862e13119b82a9f7be932b1e47
Opilane TTHK <opilane@tthk.ee>
2021-09-08 09:43:54 +0300

    HowOldAreYou

---
+++ HowOldAreYou/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HowOldAreYou
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //programm küsib kasutaja vanust
+            //programm arvutab kasutaja sünniaastat
+            //programm kuvab kasutaja sünniaastat konsoolis
+
+            //convert string from ReadLine() to int
+            Console.WriteLine("Tere, kui vana sa oled?");
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            int YearOfBirth = 2021 - Age;
+
+            Console.WriteLine("Oled sündinud aastal " + YearOfBirth);
+
+        }
+    }
+}
